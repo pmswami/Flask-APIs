3da9028005174fe503c28da4f4681857dc32b6ce
unknown <usern5949@gmail.com>
2024-03-14 11:40:51 +0530

    Added session data in flask

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
 
 app = Flask(__name__)
+app.permanent_session_lifetime = timedelta(minutes=5)
+app.secret_key = "Hello world"
 
 
 @app.route("/")
@@ -37,14 +40,34 @@ def home():
 def login():
     if request.method == "POST":
         usr = request.form["nm"]
-        return redirect(url_for("user", usr=usr))
+        session.permanent = True
+        session["user"] = usr
+        # return redirect(url_for("user", usr=usr))
+        return redirect(url_for("user"))
     else:
+        if "user" in session:
+            return redirect(url_for("user"))
         return render_template("login.html")
 
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
+
+# @app.route("/<usr>")
+# def user(usr):
+#     return f"<h1>{usr}</h1>"
+
+
+@app.route("/user")
+def user():
+    if "user" in session:
+        usr = session["user"]
+        return f"<h1>{usr}</h1>"
+    else:
+        return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
